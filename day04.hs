0f9a9707c8d52781e9149a6a2198a0dc60cd545b
Garrett Reed <garrett@airtype.com>
2015-12-04 17:17:41 -0500

    Improve day 4 type signatures

--- day04.hs
+++ day04.hs
@@ -1,13 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Data.Digest.Pure.MD5
+import Data.Digest.Pure.MD5 -- cabal install pureMD5
 import qualified Data.ByteString.Lazy.Char8 as C
 
 input = "yzbqklnj"
 
-hashIt :: String -> Integer -> String
+hashIt :: String -> Int -> String
 hashIt inp pad = show . md5 . C.pack $ inp ++ show pad
 
+zeroHash :: String -> Int -> Int -> Int
 zeroHash inp pad zeros
     | take zeros (hashIt inp pad) == replicate zeros '0' = pad
     | otherwise = zeroHash inp (pad+1) zeros
