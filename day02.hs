550c0c995098dd89e447d0701ee5299aa6e158da
Garrett Reed <garrett@airtype.com>
2015-12-04 17:16:55 -0500

    Change lwh requirement in day 2 box type

--- day02.hs
+++ day02.hs
@@ -2,24 +2,24 @@ import Data.List
 
 input = "4x23x21 22x29x19 11x4x11 8x10x5 24x18x16 11x25x22"
 
-data Box = Box { l :: Int , w :: Int , h :: Int }
+data Box = Box { s1 :: Int , s2 :: Int , s3 :: Int }
 
 sortBox :: Box -> Box
 sortBox box = Box (sorted!!0) (sorted!!1) (sorted!!2)
-    where sorted = sort [l box, w box, h box]
+    where sorted = sort [s1 box, s2 box, s3 box]
 
 splitBy :: (Eq t1, Foldable t) => t1 -> t t1 -> [[t1]]
 splitBy delimiter = foldr (\c l@(x:xs) -> if c == delimiter then []:l else (c:x):xs) [[]]
 
 area :: Box -> Int
-area box = 2 * l box * w box + 2 * w box * h box + 2 * h box * l box
+area box = 2 * s1 box * s2 box + 2 * s2 box * s3 box + 2 * s3 box * s1 box
 
 extra :: Box -> Int
-extra box = l sBox * w sBox
+extra box = s1 sBox * s2 sBox
      where sBox = sortBox box
 
 ribbon :: Box -> Int
-ribbon box = 2 * l sBox + 2 * w sBox + l sBox * w sBox * h sBox
+ribbon box = 2 * s1 sBox + 2 * s2 sBox + s1 sBox * s2 sBox * s3 sBox
     where sBox = sortBox box
 
 buildBoxes :: [Box]
